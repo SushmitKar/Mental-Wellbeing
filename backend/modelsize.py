1d004e6331bfb485d42ff90483bcc21aa9d31045
EatSushi98 <kar.sushmit@gmail.com>
2025-03-30 21:31:17 +0530

    Deleted Unnecessary Files

--- backend/modelsize.py
+++
@@ -1,5 +0,0 @@
-import os
-
-model_path = 'backend\Mood_Dataset_extracted\emotion_model.keras'  # or .h5 if using old format
-file_size = os.path.getsize(model_path) / (1024 * 1024)  # Size in MB
-print(f"Model Size: {file_size:.2f} MB")
